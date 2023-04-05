69a8d370342e452859a70148c15f203d9bb1c0e2
chingwu99 <henry88028802@hotmail.com>
2023-04-05 14:27:08 +0800

    add navigate to top button

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import MessageToast from "./components/MessageToast";
 import Loading from "./components/Loading";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const Layout = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > 500) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="layout">
       <Nav />
@@ -12,6 +34,12 @@ const Layout = () => {
       <MessageToast />
       <Outlet />
       <Footer />
+
+      <BsFillArrowUpCircleFill
+        className="navigate-to-top-button"
+        style={{ display: showButton ? "block" : "none" }}
+        onClick={() => window.scrollTo(0, 0)}
+      />
     </div>
   );
 };
